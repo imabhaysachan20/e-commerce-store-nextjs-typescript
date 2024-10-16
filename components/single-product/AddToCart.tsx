464082014035076b626660227e7e8fce47127fc6
imabhaysachan20 <150459720+imabhaysachan20@users.noreply.github.com>
2024-10-16 23:17:41 +0530

    tsc errors fixed

--- components/single-product/AddToCart.tsx
+++ components/single-product/AddToCart.tsx
@@ -1,6 +1,8 @@
 import { Button } from '../ui/button';
 
 function AddToCart({ productId }: { productId: string }) {
+  console.log(productId);
+  
   return (
     <Button className='capitalize mt-8' size='lg'>
         Add to cart
