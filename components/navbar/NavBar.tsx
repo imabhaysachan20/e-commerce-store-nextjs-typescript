cc99069949c2b1d56ff4958f62c1db12e0927a90
imabhaysachan20 <150459720+imabhaysachan20@users.noreply.github.com>
2024-10-16 23:53:24 +0530

    fallback to search

--- components/navbar/NavBar.tsx
+++ components/navbar/NavBar.tsx
@@ -4,13 +4,16 @@ import DarkMode from './DarkMode';
 import CartButton from './CartButton';
 import NavSearch from './NavSearch';
 import Container from '../global/Container';
+import { Suspense } from 'react';
 
 const NavBar = () => {
   return (
     <nav className="border-b ">
       <Container className="flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap py-8 gap-4">
       <Logo />
+      <Suspense fallback={<div>...</div>}>
         <NavSearch />
+        </Suspense>
         <div className='flex gap-4 items-center '>
           <CartButton />
           <DarkMode />
